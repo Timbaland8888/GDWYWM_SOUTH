440640f763ddba222ac4c9c8d2577848b8d8d2d3
www <422033564@qq.com>
2019-12-21 19:58:52 +0800

    add try error

--- con_mysql.py
+++ con_mysql.py
@@ -17,21 +17,26 @@ class Con_mysql():
         self.Databas = Databas
 
     def query(self,sql_query):
-        db = pymysql.connect(self.Host, self.Root,self.Pwd, self.Databas )
-        # 使用 cursor() 方法创建一个游标对象 cursor
-        cursor = db.cursor()
-        # SQL 插入语句
-        sql = sql_query
         try:
-            # 执行sql语句
-            cursor.execute(sql)
-            # 提交到数据库执行
-            db.commit()
-            result= cursor.fetchall()
-        except:
-            # 如果发生错误则回滚
-            db.rollback()
+            db = pymysql.connect(self.Host, self.Root,self.Pwd, self.Databas )
+        except Exception as e:
+            print('检查汇捷数据库')
+        finally:
 
-        # 关闭数据库连接
-        db.close()
-        return result
+            # SQL 插入语句
+            sql = sql_query
+            try:
+                # 使用 cursor() 方法创建一个游标对象 cursor
+                cursor = db.cursor()
+                # 执行sql语句
+                cursor.execute(sql)
+                # 提交到数据库执行
+                db.commit()
+                result= cursor.fetchall()
+            except:
+                # 如果发生错误则回滚
+                db.rollback()
+
+            # 关闭数据库连接
+            db.close()
+            return result
